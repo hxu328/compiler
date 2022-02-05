e865b9bf670635d8e78031287a910129f9892367
Huan Xu <huanx@royal-23.cs.wisc.edu>
2022-02-04 22:05:10 -0600

    add class header

--- p1/SymTable.java
+++ p1/SymTable.java
@@ -1,12 +1,44 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Main Class File:  P1.java
+// File:             ./P1.java
+// Semester:         CS536 Spring 2022
+//
+// Author:           Huan Xu at hxu296@wisc.edu
+// CS Login:         huanx
+// Lecturer's Name:  Beck Hasti
+// Lab Section:      001
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ////////////////////
+//
+// Pair Partner:     (name of your pair programming partner)
+// Email:            (email address of your programming partner)
+// CS Login:         (partner's login name)
+// Lecturer's Name:  (name of your partner's lecturer)
+// Lab Section:      (your partner's lab section number)
+//
+//////////////////// STUDENTS WHO GET HELP FROM OTHER THAN THEIR PARTNER //////
+//                   fully acknowledge and credit all sources of help,
+//                   other than Instructors and TAs.
+//
+// Persons:          Identify persons by name, relationship to you, and email.
+//                   Describe in detail the the ideas and help they provided.
+//
+// Online sources:   avoid web searches to solve your problems, but if you do
+//                   search, be sure to include Web URLs and description of 
+//                   of any information you find.
+//////////////////////////// 80 columns wide //////////////////////////////////
+
 /**
-*     SymTable:
-*       SymTable()           -- initialize SymTable to contain a single empty HashMap
-*       void addDecl(String name, Sym sym) throws DuplicateSymException, EmptySymTableException
-*       void addScope()      -- add a new, empty HashMap to the front of the list
-*       Sym lookupLocal(String name) throws EmptySymTableException
-*       Sym lookupGlobal(String name) throws EmptySymTableException
-*       void removeScope() throws EmptySymTableException
-*       void print()         -- print every Hashmap in the list 
+* SymTable:
+*   SymTable()           -- initialize SymTable to contain a single empty HashMap
+*   void addDecl(String name, Sym sym) throws DuplicateSymException, EmptySymTableException
+*   void addScope()      -- add a new, empty HashMap to the front of the list
+*   Sym lookupLocal(String name) throws EmptySymTableException
+*   Sym lookupGlobal(String name) throws EmptySymTableException
+*   void removeScope() throws EmptySymTableException
+*   void print()         -- print every Hashmap in the list 
+* @author Huan Xu
 */
 
 import java.util.*;
