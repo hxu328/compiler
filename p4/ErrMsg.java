dcbd10d71d47a92c2097f4c74a84cae144235c2b
Hong Xu <hongx@snares-08.cs.wisc.edu>
2022-04-06 21:24:08 -0500

    add indicator for errors occur

--- p4/ErrMsg.java
+++ p4/ErrMsg.java
@@ -4,6 +4,8 @@
  * This class is used to generate warning and fatal error messages.
  */
 class ErrMsg {
+
+    static boolean hasError = false;
     /**
      * Generates a fatal error message.
      * @param lineNum line number for error location
@@ -12,6 +14,7 @@ class ErrMsg {
      */
     static void fatal(int lineNum, int charNum, String msg) {
         System.err.println(lineNum + ":" + charNum + " ****ERROR**** " + msg);
+        hasError = true;
     }
 
     /**
